991c3652de92026dc749113c0d7fb88ad9a62fc3
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-29 12:49:24 +0300

    Add project files.

---
+++ Task6_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectionСomparison cof = new CollectionСomparison();
+            Console.WriteLine(">>>Add to collections<<<");
+            Console.WriteLine($"Speed of List: {cof.SpeedOfListAdd()}");
+            Console.WriteLine($"Speed of LinkedList: {cof.SpeedOfLinkedListAdd()}");
+            Console.WriteLine($"Speed of Dictionary: {cof.SpeedOfDictionaryAdd()}");
+            Console.WriteLine($"Speed of Queue: {cof.SpeedOfQueueAdd()}");
+            Console.WriteLine($"Speed of Stack: {cof.SpeedOfStackAdd()}");
+            Console.WriteLine($"Speed of SortedSet: {cof.SpeedOfSortedSetAdd()}");
+            Console.WriteLine($"Speed of Sorted Dictionary: {cof.SpeedOfSortedDictionaryAdd()}");
+            Console.WriteLine(">>>Read from collections<<<");
+            Console.WriteLine($"Speed of List: {cof.SpeedOfListRead()}");
+            Console.WriteLine($"Speed of LinkedList: {cof.SpeedOfLinkedListRead()}");
+            Console.WriteLine($"Speed of Dictionary: {cof.SpeedOfDictionaryRead()}");
+            Console.WriteLine($"Speed of Queue: {cof.SpeedOfQueueRead()}");
+            Console.WriteLine($"Speed of Stack: {cof.SpeedOfStackRead()}");
+            Console.WriteLine($"Speed of SortedSet: {cof.SpeedOfSortedSetRead()}");
+            Console.WriteLine($"Speed of Sorted Dictionary: {cof.SpeedOfSortedDictionaryRead()}");
+            Console.WriteLine(">>>Delete from collections<<<");
+            Console.WriteLine($"Speed of List: {cof.SpeedOfListDel()}");
+            Console.WriteLine($"Speed of LinkedList: {cof.SpeedOfLinkedListDel()}");
+            Console.WriteLine($"Speed of Dictionary: {cof.SpeedOfDictionaryDel()}");
+            Console.WriteLine($"Speed of Queue: {cof.SpeedOfQueueDel()}");
+            Console.WriteLine($"Speed of Stack: {cof.SpeedOfStackDel()}");
+            Console.WriteLine($"Speed of SortedSet: {cof.SpeedOfSortedSetDel()}");
+            Console.WriteLine($"Speed of Sorted Dictionary: {cof.SpeedOfSortedDictionaryDel()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6_1/СomparisonOfSpeed.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_1
+{
+    public class CollectionСomparison
+    {
+        List<int> list = new List<int>();
+        LinkedList<int> linkedList = new LinkedList<int>();
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        Queue<int> queue = new Queue<int>();
+        Stack<int> stack = new Stack<int>();
+        SortedSet<int> sortedSet = new SortedSet<int>();
+        SortedDictionary<int, int> sortedDictionary = new SortedDictionary<int, int>();
+        public long SpeedOfListAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                list.Add(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfListRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                int buf = list[i];
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfListDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                list.Remove(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfLinkedListAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                linkedList.AddLast(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfLinkedListRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                int buf = linkedList.ElementAt(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfLinkedListDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                linkedList.Remove(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfDictionaryAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                dictionary.Add(i,i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfDictionaryRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+
+                int buf = dictionary[i];
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfDictionaryDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                dictionary.Remove(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfQueueAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                queue.Enqueue(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfQueueRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                int buf = queue.ElementAt(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfQueueDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                queue.Dequeue();
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfStackAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();  
+            for (int i = 0; i < 100000; i++)
+            {
+                stack.Push(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfStackRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+               int buf = stack.ElementAt(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfStackDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                stack.Pop();
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedSetAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                sortedSet.Add(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedSetRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                int buf=sortedSet.ElementAt(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedSetDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                sortedSet.Remove(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedDictionaryAdd()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                sortedDictionary.Add(i, i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedDictionaryRead()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                sortedDictionary.ElementAt(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+        public long SpeedOfSortedDictionaryDel()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 100000; i++)
+            {
+                sortedDictionary.Remove(i);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+    }
+}
